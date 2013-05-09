d0587247c7784aa5a64da0b4036fb61475233128
zofe <felice.ostuni@gmail.com>
2013-05-09 07:58:24 +0200

    integrated twitter bootstrap (remote source),  reimplemented demo: basic controller.

--- src/Modules/Demos/Controllers/Basic.php
+++ src/Modules/Demos/Controllers/Basic.php
@@ -1,20 +1,18 @@
 <?php
 
-
 namespace Modules\Demos\Controllers;
-
 
 class Basic extends \Rapyd\Controller
 {
 
     public function indexAction()
     {
-		$data['title'] 	   = 'Basic Controller';
-        $data['active']    = 'basic';
+        $data['title'] = 'Basic Controller';
+        $data['active'] = 'basic';
         $data['content'] = 'Hello World!';
-        $data['code']    = highlight_string(file_get_contents(__FILE__), TRUE);
-        
+        $data['code'] = highlight_string(file_get_contents(__FILE__), TRUE);
+
         $this->render('Demo', $data);
-	}
+    }
 
 }
