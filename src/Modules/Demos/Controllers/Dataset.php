0789b9d12e4cf1ae3606ecad42561cbbe05074bb
zofe <felice.ostuni@gmail.com>
2013-05-11 11:28:16 +0200

    fix indentation tab to spaces

--- src/Modules/Demos/Controllers/Dataset.php
+++ src/Modules/Demos/Controllers/Dataset.php
@@ -14,11 +14,11 @@ class Dataset extends \Rapyd\Controller
         $ds->num_links= 2;
         $ds->build();   
 
-        $data['title'] = 'Schema Builder';
+        $data['title'] = 'DataSet Widget';
         $data['active'] = 'dataset';
         $data['content_raw'] = $this->fetch('Dataset', array('ds' => $ds));
-        $data['code'] = highlight_string(file_get_contents(__FILE__), TRUE);
-
+        $data['code']  = highlight_string(file_get_contents(__FILE__), TRUE);
+        $data['code'] .= htmlentities(file_get_contents(__DIR__.'/../Views/Dataset.twig'));
         $this->render('Demo', $data);
     }
 
