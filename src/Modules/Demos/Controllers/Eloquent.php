dd5d9e7d27944c2d64466da0f72934f045bb9b28
zofe <felice.ostuni@gmail.com>
2013-11-17 09:18:23 +0100

    fix

--- src/Modules/Demos/Controllers/Eloquent.php
+++ src/Modules/Demos/Controllers/Eloquent.php
@@ -12,7 +12,7 @@ class Eloquent extends \Rapyd\Controller
     public function indexAction()
     {
         //get first article using an eloquent model
-        $article = Article::with("comments", "author")->find(1);
+        $article = Article::with("author")->find(1);
 
         $this->render('Eloquent', array('article' => $article));
     }
